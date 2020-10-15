1d624313c6fa5a3b5831fda2f1b33833802e4697
Manish <manishkartiks@gmail.com>
2020-10-15 14:44:35 +0530

    this is customer pojo class

---
+++ MyApp/src/main/java/com/ofss/Customer.java
@@ -0,0 +1,30 @@
+package com.ofss;
+
+public class Customer {
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + "]";
+	}
+
+	public Customer(String firstName) {
+		super();
+		this.firstName = firstName;
+	}
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
